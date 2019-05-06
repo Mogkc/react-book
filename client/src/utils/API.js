59607c6de488230c1658a228b07aecb44b7c962d
Mogkc <carson.mogk@cox.net>
2019-05-06 13:15:24 -0700

    added axios and return

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,7 +1,9 @@
+import axios from 'axios';
+
 const key = process.env.API_Key;
 
 export default (searchParams) => {
     searchParams.replace(" ", "+");
     const searchURL = `https://www.googleapis.com/books/v1/volumes?q=${searchParams}&key=${key}`;
-    // Use axios to make the api call to this address
+    return axios.get(searchURL);
 }
