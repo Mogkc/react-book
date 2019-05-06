e0d0ae51cb205cea8514eac673a3678b63d40360
Mogkc <carson.mogk@cox.net>
2019-05-06 12:04:29 -0700

    added react router with empty pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,14 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <Route exact path="/" component={Search} />
+        <Route exact path="/saved" component={Saved} />
+      </Router>
     );
   }
 }
